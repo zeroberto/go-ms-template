7ea3c771908edcc87100b0d73caf84d123171437
Ze Roberto <roberto.jrnj@gmail.com>
2020-08-09 18:58:54 -0300

    UpdateExampleProperties implementation

--- usecase/creation/exampleCreationUseCase.go
+++ usecase/creation/exampleCreationUseCase.go
@@ -1,8 +1,11 @@
 package creation
 
 import (
+	"fmt"
+
 	"github.com/zeroberto/go-ms-template/dataservice"
 	"github.com/zeroberto/go-ms-template/model"
+	"github.com/zeroberto/go-ms-template/tool"
 	"github.com/zeroberto/go-ms-template/usecase"
 )
 
@@ -13,7 +16,7 @@ type ExampleCreationUseCaseImpl struct {
 
 // CreateExample is responsible for creating a new Example
 func (ecuc *ExampleCreationUseCaseImpl) CreateExample(example *model.Example) (*model.Example, error) {
-	if err := ecuc.existsByName(example.Name); err != nil {
+	if err := ecuc.existsByName(example.Name, example.ID); err != nil {
 		return nil, err
 	}
 	example, err := ecuc.EDS.Create(example)
@@ -25,10 +28,10 @@ func (ecuc *ExampleCreationUseCaseImpl) CreateExample(example *model.Example) (*
 
 // UpdateExample is responsible for updating the complete Example model
 func (ecuc *ExampleCreationUseCaseImpl) UpdateExample(example *model.Example) (*model.Example, error) {
-	if err := ecuc.existsByName(example.Name); err != nil {
+	if err := ecuc.notExistsByID(example.ID); err != nil {
 		return nil, err
 	}
-	if err := ecuc.notExistsByID(example.ID); err != nil {
+	if err := ecuc.existsByName(example.Name, example.ID); err != nil {
 		return nil, err
 	}
 	example, err := ecuc.EDS.Update(example)
@@ -39,16 +42,37 @@ func (ecuc *ExampleCreationUseCaseImpl) UpdateExample(example *model.Example) (*
 }
 
 // UpdateExampleProperties is responsible for updating partial properties of the Example model
-func (ecuc *ExampleCreationUseCaseImpl) UpdateExampleProperties(example *model.Example) (*model.Example, error) {
-	return nil, nil
+func (ecuc *ExampleCreationUseCaseImpl) UpdateExampleProperties(ID int64, properties map[string]interface{}) (*model.Example, error) {
+	if err := ecuc.notExistsByID(ID); err != nil {
+		return nil, err
+	}
+	propertyNames := getUpgradeableProperties()
+	for k := range properties {
+		if !tool.ContainsString(k, propertyNames) {
+			return nil, &usecase.Error{Message: fmt.Sprintf("property %s does not exist or cannot be updated", k)}
+		}
+	}
+	if tool.ContainsStringKey("Name", properties) {
+		if err := ecuc.existsByName(properties["Name"].(string), ID); err != nil {
+			return nil, err
+		}
+	}
+	if err := ecuc.EDS.UpdateProperties(ID, properties); err != nil {
+		return nil, &usecase.Error{Cause: err}
+	}
+	example, err := ecuc.EDS.FindByID(ID)
+	if err != nil {
+		return nil, &usecase.Error{Cause: err}
+	}
+	return example, nil
 }
 
-func (ecuc *ExampleCreationUseCaseImpl) existsByName(name string) error {
+func (ecuc *ExampleCreationUseCaseImpl) existsByName(name string, ID int64) error {
 	example, err := ecuc.EDS.FindByName(name)
 	if err != nil {
 		return &usecase.Error{Cause: err}
 	}
-	if example != nil {
+	if example != nil && example.ID != ID {
 		return &usecase.Error{Message: "Example already exists"}
 	}
 	return nil
@@ -63,4 +87,8 @@ func (ecuc *ExampleCreationUseCaseImpl) notExistsByID(ID int64) error {
 		return &usecase.Error{Message: "No example found for this ID"}
 	}
 	return nil
+}
+
+func getUpgradeableProperties() []string {
+	return []string{"Name", "Useful"}
 }
